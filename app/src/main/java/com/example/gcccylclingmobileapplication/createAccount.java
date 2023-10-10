b377783d181d3291ff2f0f8f4a2de706fb4161e4
Andrew Maciel <amaci015uottawa.ca>
2023-10-10 13:57:30 -0400

    Fixing login.xml and adding firebase to the project.

---
+++ app/src/main/java/com/example/gcccylclingmobileapplication/createAccount.java
@@ -0,0 +1,17 @@
+package com.example.gcccylclingmobileapplication;
+
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+public class createAccount {
+    public Result<LoggedInUser> register(String username,String password, String email, String role){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference newUserRoleRef = database.getReference("users/"+username+"/role");
+        DatabaseReference newUserEmailRef = database.getReference("users/"+username+"/email");
+        DatabaseReference newUserPasswordRef = database.getReference("users/"+username+"/password");
+
+        newUserRoleRef.setValue(role);
+        newUserEmailRef.setValue(email);
+        newUserPasswordRef.setValue(password);
+    }
+}
