a9928702ac44b426ee66acd8299227a44b2a450e
Kai <34955546+st4rt-up@users.noreply.github.com>
2023-10-10 18:31:49 -0400

    Added account class

---
+++ app/src/main/java/com/example/gcccylclingmobileapplication/Account.java
@@ -0,0 +1,29 @@
+package com.example.gcccylclingmobileapplication;
+
+import com.google.firebase.ktx.Firebase;
+
+public class Account {
+
+    String message;
+    String firstName;
+    String username;
+    String password;
+
+    public boolean logIn() {
+        return false;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+
+}
