85779934ffd89898ec7926c432ee7d550b4f9f94
Andrew Maciel <amaci015uottawa.ca>
2023-10-10 21:32:12 -0400

    Fixing the createAccount file and certain files being named with innapropriate characters. Adding button function to register.

--- app/src/main/java/com/example/gcccylclingmobileapplication/createAccount.java
+++ app/src/main/java/com/example/gcccylclingmobileapplication/createAccount.java
@@ -1,26 +1,74 @@
 package com.example.gcccylclingmobileapplication;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class createAccount {
+public class createAccount extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText emailEditText, passwordEditText, usernameEditText;
 
-    private LoggedInUser user = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
 
-    private void setLoggedInUser(LoggedInUser user){
-        this.user = user;
+        mAuth = FirebaseAuth.getInstance();
+        emailEditText = findViewById(R.id.email);
+        passwordEditText = findViewById(R.id.password);
+        usernameEditText = findViewById(R.id.username);
+
+        Button registerButton = findViewById(R.id.signup_button);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get the email, password, and username from the EditText fields
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                String username = usernameEditText.getText().toString();
+
+                // Call the registerUser method with the email, password, and username
+                registerUser(email, password, username);
+            }
+        });
     }
-    public Result<LoggedInUser> register(String username,String password, String email, String role){
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference newUserRoleRef = database.getReference("users/"+username+"/role");
-        DatabaseReference newUserEmailRef = database.getReference("users/"+username+"/email");
-        DatabaseReference newUserPasswordRef = database.getReference("users/"+username+"/password");
 
-        newUserRoleRef.setValue(role);
-        newUserEmailRef.setValue(email);
-        newUserPasswordRef.setValue(password);
+    private void registerUser(String email, String password, String username) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            if (user != null) {
+                                String userId = user.getUid();
 
+                                // Save additional user information to Firebase Realtime Database
+                                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                DatabaseReference usersRef = database.getReference("users");
 
-        return register;
+                                // Save email and username to the database
+                                DatabaseReference newUserRef = usersRef.child(userId);
+                                newUserRef.child("email").setValue(email);
+                                newUserRef.child("username").setValue(username);
+
+                                // Registration success
+                            }
+                        } else {
+                            // Registration failed
+
+                        }
+                    }
+                });
     }
 }
