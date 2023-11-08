bf4f41d0d3bcee59251cb7e51e916234d7a9d3be
Andrew Maciel <amaci015uottawa.ca>
2023-11-08 13:15:20 -0500

    Adding the new EventList Layout and code

--- app/src/main/java/com/example/gcccylclingmobileapplication/AccountManager.java
+++ app/src/main/java/com/example/gcccylclingmobileapplication/AccountManager.java
@@ -3,9 +3,12 @@ package com.example.gcccylclingmobileapplication;
 public class AccountManager {
     public void createAccount(Account account) {}
 
-    public Account getAccount(String uid) {}
-
-    public boolean deleteAccount(String uid) {}
+    public Account getAccount(String uid) {
+        return null;
+    }
+    public boolean deleteAccount(String uid) {
+        return false;
+    }
 
     public void logIn(String uid) {}
 
