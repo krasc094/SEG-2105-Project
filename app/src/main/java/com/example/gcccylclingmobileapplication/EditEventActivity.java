fa5402bf990e28beaf5f7ad9ae2fabc54f519f01
Andrew Maciel <amaci015uottawa.ca>
2023-11-10 13:57:46 -0500

    renaming class

--- app/src/main/java/com/example/gcccylclingmobileapplication/EditEventActivity.java
+++ app/src/main/java/com/example/gcccylclingmobileapplication/EditEventActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class EditEvent extends AppCompatActivity {
+public class EditEventActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
