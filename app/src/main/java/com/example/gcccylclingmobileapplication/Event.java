6e739678b661f54a7d695832320a92145fbbc9fe
Jordan Burgess <jburg100@uottawa.ca>
2023-11-10 13:55:07 -0500

    delete event & edit event added

--- app/src/main/java/com/example/gcccylclingmobileapplication/Event.java
+++ app/src/main/java/com/example/gcccylclingmobileapplication/Event.java
@@ -1,16 +1,19 @@
 package com.example.gcccylclingmobileapplication;
 
 public class Event {
+    String eid;
     String name;
     String type;
-    String requirements;
+    String[] requirements;
 
     // getters and setters
     public void setName(String name) { this.name = name; }
     public void setType(String type) { this.type = type; }
-    public void setRequirements(String requirements) { this.requirements = requirements; }
+    public void setRequirements(String[] requirements) { this.requirements = requirements; }
+    public void setEID(String eid) { this.eid = eid; }
 
     public String getName() { return name; }
     public String getType() { return type; }
-    public String getRequirements() { return requirements; }
+    public String[] getRequirements() { return requirements; }
+    public String getEID() { return eid; }
 }
--- app/src/main/java/com/example/gcccylclingmobileapplication/EventManager.java
+++ app/src/main/java/com/example/gcccylclingmobileapplication/EventManager.java
@@ -18,6 +18,7 @@ public class EventManager {
 
         //possibly working.
 
+        // missing arguments, createEvent fails
         boolean anyInvalidFields = event.getName() == null || event.getType() == null || event.getRequirements() == null;
         if (anyInvalidFields) {
             // missing arguments, createAccount fails
@@ -26,23 +27,42 @@ public class EventManager {
             return;
         }
 
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance(); // get ref to firebase auth server
 
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference("events");
+        DatabaseReference ref = database.getReference("events"); // get reference to '/events' directory
 
-        Map<String, Event> events = new HashMap<>();
-        events.put(event.getName(), event);
+        Map<String, Event> events = new HashMap<>(); // create local hash map
+        events.put(event.getEID(), event); // put data in local hash map as / <UID> -> account
 
-        ref.child(event.getName()).setValue(event);
+        ref.child(event.getEID()).setValue(events);// put hash map in database at /events/ directory
     }
 
-    public void deleteEvent() {
-        // implement later
+    public void deleteEvent(Event event) {
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference eventRef = database.getReference("events/" + event.getEID());
+
+        eventRef.removeValue();
     }
 
-    public void editEvent() {
-        // implement later
+    public void editEvent(Event event, String newName, String newType, String[] newRequirements) {
+
+        //Setting up reference to specific event in database
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference eventRef = database.getReference("events/" + event.getEID());
+
+        //Updating the event
+        event.setName(newName);
+        event.setType(newType);
+        event.setRequirements(newRequirements);
+
+        //Creating new hashmap with new values
+        Map<String, Object> eventUpdates = new HashMap<>();
+        eventUpdates.put(event.getEID(), event);
+
+        //Committing new updates to database
+        eventRef.child(event.getEID()).setValue(event);
     }
 
 }
