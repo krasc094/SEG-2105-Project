1615c25917db136128523536f83a213d22a6cac3
Andrew Maciel <amaci015uottawa.ca>
2023-11-10 23:51:37 -0500

    final changes

--- app/src/main/java/com/example/gcccylclingmobileapplication/ui/EditEventActivity.java
+++ app/src/main/java/com/example/gcccylclingmobileapplication/ui/EditEventActivity.java
@@ -1,8 +1,10 @@
-package com.example.gcccylclingmobileapplication;
+package com.example.gcccylclingmobileapplication.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.gcccylclingmobileapplication.R;
 
 public class EditEventActivity extends AppCompatActivity {
 
