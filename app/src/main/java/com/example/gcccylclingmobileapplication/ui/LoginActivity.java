385787857c06a8f1199cdfa2e37cc5693ee7f4ef
Jordan Burgess <jburg100@uottawa.ca>
2023-12-02 16:30:52 -0500

    Set onClick function for register button in LoginActivity.java

--- app/src/main/java/com/example/gcccylclingmobileapplication/ui/LoginActivity.java
+++ app/src/main/java/com/example/gcccylclingmobileapplication/ui/LoginActivity.java
@@ -5,21 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.gcccylclingmobileapplication.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    Button signUpButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        signUpButton = findViewById(R.id.btn_register);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onRegisterButtonClick();
+            }
+        });
     }
 
-    public void onRegisterButtonClick(View view){
+    public void onRegisterButtonClick(){
         Intent intent = new Intent(this, SignupActivity.class);
         startActivity(intent);
     }
