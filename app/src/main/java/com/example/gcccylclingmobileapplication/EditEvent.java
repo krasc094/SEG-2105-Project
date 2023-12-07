a6ba2db00d53cfec5f4f8e7b66c0e65618dd12fd
KrisKeon <kkeon022@uottawa.ca>
2023-12-06 23:58:14 -0500

    Last commit  All UI are done. fixed naming

--- app/src/main/java/com/example/gcccylclingmobileapplication/EditEvent.java
+++ app/src/main/java/com/example/gcccylclingmobileapplication/EditEvent.java
@@ -9,6 +9,6 @@ public class EditEvent extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_event);
+        setContentView(R.layout.activity_find_event);
     }
 }
