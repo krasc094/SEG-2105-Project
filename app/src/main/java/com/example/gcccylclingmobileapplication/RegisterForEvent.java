46703dfffb3a7fe5fdd41193531afb6430300f64
KrisKeon <kkeon022@uottawa.ca>
2023-12-06 23:49:32 -0500

    Last commit  All UI are done

---
+++ app/src/main/java/com/example/gcccylclingmobileapplication/RegisterForEvent.java
@@ -0,0 +1,14 @@
+package com.example.gcccylclingmobileapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterForEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_for_event);
+    }
+}
